e022a1abaebf5275816ce4c9ae4efc9c7e2d37ff
alexhenson <chhiengalex@gmail.com>
2021-12-04 13:20:25 -0600

    ReportCtrl onActionCust

--- src/controller/ReportsCtrl.java
+++ src/controller/ReportsCtrl.java
@@ -1,9 +1,32 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import model.Appointment;
+import tools.StaticObservableLists;
 
-public class ReportsCtrl {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReportsCtrl implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
     public void onActionCust(ActionEvent actionEvent) {
+        int countPlanning = 0;
+        int countDebrief = 0;
+        int countFinancial = 0;
+        int countBrainstorm = 0;
+        int countCareer = 0;
+        int[] countByMonth = new int[12];
+
+        for (Appointment a : StaticObservableLists.appointmentList) {
+            if
+        }
+        String str =
+
     }
 
     public void onActionMainMenu(ActionEvent actionEvent) {
