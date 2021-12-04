4619d2e681c6a7ea5083c15370c1f1d0f2f82101
alexhenson <chhiengalex@gmail.com>
2021-12-04 01:44:41 -0600

    finished Report fmxl gui

--- src/controller/ReportsCtrl.java
+++ src/controller/ReportsCtrl.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class ReportsCtrl {
+    public void onActionCust(ActionEvent actionEvent) {
+    }
+
+    public void onActionMainMenu(ActionEvent actionEvent) {
+    }
+
+    public void onActionAppt(ActionEvent actionEvent) {
+    }
 }
